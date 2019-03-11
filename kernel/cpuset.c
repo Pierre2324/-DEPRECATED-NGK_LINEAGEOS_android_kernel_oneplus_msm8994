2253e0490f055048eb28d1b53bf0a6cd2172da51
Pierre2324 <pdbbasketball@gmail.com>
2019-03-11 12:25:46 -0400

    cpuset: fix force_rebuild function duplicate

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -2132,13 +2132,6 @@ static void schedule_cpuset_propagate_hotplug(struct cpuset *cs)
 		css_put(&cs->css);
 }
 
-static bool force_rebuild;
-
-void cpuset_force_rebuild(void)
-{
-	force_rebuild = true;
-}
-
 /**
  * cpuset_hotplug_workfn - handle CPU/memory hotunplug for a cpuset
  *
