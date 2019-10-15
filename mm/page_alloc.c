04221806e466a607a313d1761f1b71f600ab3f9a
Pierre2324 <pdbbasketball@gmail.com>
2019-10-14 22:10:14 -0400

    mm: Fix Boost use on our kernel

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -61,7 +61,7 @@
 #include <linux/page-debug-flags.h>
 #include <linux/hugetlb.h>
 #include <linux/sched/rt.h>
-#include <linux/cpu_input_boost.h>
+#include <linux/cpu_boost.h>
 
 #include <asm/sections.h>
 #include <asm/tlbflush.h>
@@ -2659,7 +2659,7 @@ rebalance:
 	}
 
 	/* Boost when memory is low so allocation latency doesn't get too bad */
-	cpu_input_boost_kick_max(100);
+	do_input_boost_max();
 
 	/* Check if we should retry the allocation */
 	pages_reclaimed += did_some_progress;
